43e5f801fb12e1df3fe74900ea95a221fd8fa53b
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-10-28 08:52:19 +0200

    Add Post and Thread models with factories and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\Thread;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Thread::factory(3)->create()->each(function ($thread) {
+            Post::factory(10)->create(['thread_id' => $thread->id]);
+        });
     }
 }
