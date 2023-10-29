faeddd57e3a536b70dcda5c3785a7738e5dbf117
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-10-29 16:23:44 +0100

    Add routes for login, signup, and posts

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts/{threadId}', [PostController::class, 'index']);
+
+Route::get('/signup', [SignupController::class, 'showSignupForm']);
+
+Route::post('/signup', [SignupController::class, 'signup']);
+
+Route::get('/login', [LoginController::class, 'showLoginForm']);
+
+Route::post('/login', [LoginController::class, 'login']);
