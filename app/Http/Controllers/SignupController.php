39c396d479afe752738beaa1a2f3f7e25f93f6ab
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-10-29 16:23:15 +0100

    Add login and signup controllers

---
+++ app/Http/Controllers/SignupController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class SignupController extends Controller
+{
+    public function showSignupForm()
+    {
+        return view('signup.index');
+    }
+
+    public function signup(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+
+        $formFields['password'] = bcrypt($formFields['password']);
+
+
+        $user = User::create($formFields);
+
+        return redirect('login')->with('success', 'Account created successfully!');
+    }
+}
