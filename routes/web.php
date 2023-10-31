ac4f97bc2da39609e5dc34a527c93337d70f7543
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-10-31 12:40:49 +0100

    Add middleware

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,25 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/signup', [SignupController::class, 'showSignupForm']);
 
-Route::post('/signup', [SignupController::class, 'signup']);
 
-Route::get('/login', [LoginController::class, 'showLoginForm']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+});
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/signup', [SignupController::class, 'showSignupForm'])->name('signup');
+
+    Route::post('/signup', [SignupController::class, 'signup']);
+
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+
+    Route::post('/login', [LoginController::class, 'login']);
+});
