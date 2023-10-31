2997f9b6ce0a0923c06694a3354f125d2e2114ba
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-10-31 11:59:42 +0100

    Add nav bar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,13 @@
 </head>
 
 <body>
-    {{ $slot }}
+    @auth
+        <x-nav-bar />
+    @endauth
+
+    <div class="container">
+        {{ $slot }}
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
---
+++ resources/views/components/nav-bar.blade.php
@@ -0,0 +1,4 @@
+<nav class="navbar p-2 md-p-5">
+    <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
+    <x-logout-btn />
+</nav>
