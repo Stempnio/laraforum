1dc8723d9c4c246e5af5a4d2c5f16cd71a2e1196
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-10-31 14:20:26 +0100

    Update repositories

--- app/Repositories/Eloquent/PostRepository.php
+++ app/Repositories/Eloquent/PostRepository.php
@@ -1,16 +1,12 @@
 <?php
+
 namespace App\Repositories\Eloquent;
+
 use App\Models\Post;
 use App\Repositories\PostRepositoryInterface;
-use Illuminate\Support\Collection;
 
 class PostRepository implements PostRepositoryInterface
 {
-    public function getThreadPosts(int $threadId): Collection
-    {
-        return Post::where('thread_id', $threadId)->get();
-    }
-
     public function create(int $threadId, int $userId, string $content): void
     {
         Post::create([
--- app/Repositories/PostRepositoryInterface.php
+++ app/Repositories/PostRepositoryInterface.php
@@ -1,10 +1,10 @@
 <?php
+
 namespace App\Repositories;
+
 use Illuminate\Support\Collection;
 
 interface PostRepositoryInterface
 {
-    public function getThreadPosts(int $threadId): Collection;
-
     public function create(int $threadId, int $userId, string $content);
 }
