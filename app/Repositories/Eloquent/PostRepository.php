ef178ff856740e402f19aac9e01d7ce1cda71aae
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-11-03 12:25:32 +0100

    Update thread on post add

--- app/Repositories/Eloquent/PostRepository.php
+++ app/Repositories/Eloquent/PostRepository.php
@@ -3,16 +3,22 @@
 namespace App\Repositories\Eloquent;
 
 use App\Models\Post;
+use App\Models\Thread;
 use App\Repositories\PostRepositoryInterface;
 
 class PostRepository implements PostRepositoryInterface
 {
     public function create(int $threadId, int $userId, string $content): void
     {
-        Post::create([
+        $post = Post::create([
             'thread_id' => $threadId,
             'user_id' => $userId,
             'content' => $content,
         ]);
+
+        if ($post) {
+            $thread = Thread::find($threadId);
+            $thread->touch();
+        }
     }
 }
